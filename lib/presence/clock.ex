3585ed34ee1039c75ab2c4f4c6c3b5b4caa89aaf
Alex Songe <a@songe.me>
2016-01-17 17:08:22 -0600

    More edge cases for presence...

--- lib/presence/clock.ex
+++ lib/presence/clock.ex
@@ -1,19 +1,32 @@
 defmodule Presence.Clock do
 
   def append_clock(clockset, clock) do
+    big_clock = combine_clocks(clockset)
+    cond do
+      dominates?(clock, big_clock) -> [clock]
+      dominates?(big_clock, clock) -> clockset
+      true -> filter_clocks(clockset, clock)
+    end
+  end
+
+  defp filter_clocks(clockset, clock) do
     # The acc in the reduce is a tuple of the new clockset, and the current status
     clockset
-    |> Enum.reduce({[], false}, fn clock2, {set, replace} ->
+    |> Enum.reduce({[], false}, fn clock2, {set, insert} ->
       if dominates?(clock, clock2) do
         {set, true}
       else
-        {[clock2|set], replace}
+        {[clock2|set], insert || !dominates?(clock2, clock)}
       end
     end)
     |> case do
       {new_clockset, true} -> [clock|new_clockset]
       {new_clockset, false} -> new_clockset
     end
+  end
+
+  defp combine_clocks(clockset) do
+    Enum.reduce(clockset, %{}, &Map.merge(&1, &2, fn _,a,b -> max(a,b) end))
   end
 
   # Really fast short-circuit that is just too easy to pass up
--- test/presence_clock_test.exs
+++ test/presence_clock_test.exs
@@ -15,10 +15,11 @@ defmodule PresenceClockTest do
     clock1 = %{a: 1, b: 2, c: 3}
     clock2 = %{b: 2, c: 3, d: 1}
     clock3 = %{a: 1, b: 2}
-    assert [clock1, clock2] == Clock.append_clock([clock2, clock3], clock1) |> Enum.sort
+    assert [clock3, clock2] == Clock.append_clock([clock2, clock3], clock1) |> Enum.sort
     assert [clock1, clock2] == Clock.append_clock([clock1, clock2], clock3) |> Enum.sort
     assert [clock1, clock2] == Clock.append_clock([clock1, clock2], clock1) |> Enum.sort
     assert [clock1, clock2] == Clock.append_clock([clock1, clock2], clock2) |> Enum.sort
+    assert [clock3, clock1, clock2] == Clock.append_clock([clock1, clock3], clock2) |> Enum.sort
   end
 
 end
