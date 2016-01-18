f8bac2cf4f7bea753eaf652f40c5d361ed7b691d
Alex Songe <a@songe.me>
2016-01-17 21:11:09 -0600

    clocksets are {node,clocks} pairs now

--- lib/presence/clock.ex
+++ lib/presence/clock.ex
@@ -1,32 +1,38 @@
 defmodule Presence.Clock do
 
-  def append_clock(clockset, clock) do
+  def clockset_nodes(clockset) do
+    for {node, _} <- clockset, do: node
+  end
+
+  def append_clock(clockset, {node, clock}) do
     big_clock = combine_clocks(clockset)
     cond do
-      dominates?(clock, big_clock) -> [clock]
+      dominates?(clock, big_clock) -> [{node, clock}]
       dominates?(big_clock, clock) -> clockset
-      true -> filter_clocks(clockset, clock)
+      true -> filter_clocks(clockset, {node, clock})
     end
   end
 
-  defp filter_clocks(clockset, clock) do
+  defp filter_clocks(clockset, {node, clock}) do
     # The acc in the reduce is a tuple of the new clockset, and the current status
     clockset
-    |> Enum.reduce({[], false}, fn clock2, {set, insert} ->
+    |> Enum.reduce({[], false}, fn {node2, clock2}, {set, insert} ->
       if dominates?(clock, clock2) do
         {set, true}
       else
-        {[clock2|set], insert || !dominates?(clock2, clock)}
+        {[{node2, clock2}|set], insert || !dominates?(clock2, clock)}
       end
     end)
     |> case do
-      {new_clockset, true} -> [clock|new_clockset]
+      {new_clockset, true} -> [{node, clock}|new_clockset]
       {new_clockset, false} -> new_clockset
     end
   end
 
   defp combine_clocks(clockset) do
-    Enum.reduce(clockset, %{}, &Map.merge(&1, &2, fn _,a,b -> max(a,b) end))
+    clockset
+    |> Enum.map(fn {_, clocks} -> clocks end)
+    |> Enum.reduce(%{}, &Map.merge(&1, &2, fn _,a,b -> max(a,b) end))
   end
 
   # Really fast short-circuit that is just too easy to pass up
--- test/presence_clock_test.exs
+++ test/presence_clock_test.exs
@@ -12,14 +12,16 @@ defmodule PresenceClockTest do
   end
 
   test "test the set trims..." do
-    clock1 = %{a: 1, b: 2, c: 3}
-    clock2 = %{b: 2, c: 3, d: 1}
-    clock3 = %{a: 1, b: 2}
-    assert [clock3, clock2] == Clock.append_clock([clock2, clock3], clock1) |> Enum.sort
+    clock1 = {:a, %{a: 1, b: 2, c: 3}}
+    clock2 = {:b, %{b: 2, c: 3, d: 1}}
+    clock3 = {:c, %{a: 1, b: 2}}
+    assert [clock2, clock3] == Clock.append_clock([clock2, clock3], clock1) |> Enum.sort
     assert [clock1, clock2] == Clock.append_clock([clock1, clock2], clock3) |> Enum.sort
     assert [clock1, clock2] == Clock.append_clock([clock1, clock2], clock1) |> Enum.sort
     assert [clock1, clock2] == Clock.append_clock([clock1, clock2], clock2) |> Enum.sort
-    assert [clock3, clock1, clock2] == Clock.append_clock([clock1, clock3], clock2) |> Enum.sort
+    assert [clock1, clock2, clock3] == Clock.append_clock([clock1, clock3], clock2) |> Enum.sort
+    # This is how you get the nodes back you need to request:
+    assert [:b, :c] == Clock.append_clock([clock2, clock3], clock1) |> Clock.clockset_nodes |> Enum.sort
   end
 
 end
